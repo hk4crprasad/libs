d9bc91fede5927a0712b872f449007b0cbd10bf3
SILENCER <82955298+hk4crprasad@users.noreply.github.com>
2023-03-04 21:27:54 +0530

    Add files via upload

---
+++ piro.sh
@@ -0,0 +1,532 @@
+NOCOLOR='\e[0m'
+RED='\e[31m'
+GREEN='\e[32m'
+ORANGE='\e[\em'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+LIGHTGRAY='\e[37m'
+DARKGRAY='\e[30m'
+LIGHTRED='\e[31m'
+LIGHTGREEN='\e[32m'
+YELLOW='\e[\em'
+LIGHTBLUE='\e[34m'
+LIGHTPURPLE='\e[35m'
+LIGHTCYAN='\e[36m'
+WHITE='\e[37m'
+BOLD='\e[1m'
+NOPE='\e[m'
+BLINK='\e[5m'
+gith="https://github.com"
+spin() {
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo""
+echo""
+echo""
+echo""
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                /\e[36m█\e[m1%\e[31m......................\e[m/\e[m"
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                /\e[36m█\e[m4%\e[31m......................\e[m/\e[m"
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                /\e[36m█\e[m8%\e[31m......................\e[m/\e[m"
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                /\e[36m██\e[m10%\e[31m....................\e[m/\e[m"
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                /\e[36m██\e[m12%\e[31m....................\e[m/\e[m"
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                /\e[36m███\e[m15%\e[31m..................\e[m/\e[m"
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                /\e[36m█████\e[m19%\e[31m..............\e[m/\e[m"
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                /\e[36m███████\e[m28%\e[31m..........\e[m/\e[m"
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                "/████████30%.................../
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                "/██████████40%................/
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                "/█████████████50%............./
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                "/████████████████60%........../
+sleep 0.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e '
+               \e[33m┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+               \e[33m│  │ │├─┤ │││││││ ┬
+               \e[33m┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘\e[m
+'
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e "                "/███████████████████88%......./
+sleep 2.0
+clear
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+echo -e " "
+clear
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo '
+_                    _
+| |                  | |
+| |__   ___  __ _ ___| |_
+|  _ \ / _ \/ _` / __| __|
+| |_) |  __/ (_| \__ \ |_
+|_.__/ \___|\__,_|___/\__|'
+echo -e ""
+echo -e ""
+echo -e "               "[██████████████████████ 100%]
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+sleep 1.0
+}
+quit() {
+echo -e " "
+echo -e " "
+echo -e "\e[1;33;4;44mCREDIT \e[m:- @HK4CRPRASAD\e[m"
+echo -e "${LIGHTBLUE} TELEGRAM ${WHITE}:- ${LIGHTRED}@ROYALANTIBAN\e[m"
+echo -e "${RED} FUCK YOUR MOM COPY PASTER\e[m"
+echo -e "\e[1;32;4;41m TRY TO MAKE YOUR OWN, DONT SPOIL MY CREATIONS${WHITE}\e[m"
+}
+srk() {
+curl -O https://raw.githubusercontent.com/hk4crprasad/password/master/version > /dev/null 2>&1
+source version
+pwr="${version}"
+rm version
+}
+srk
+spin
+killk() {
+echo -e "
+${RED}██\e[m╗      ${RED}██████\e[m╗  ${RED}██████\e[m╗  ${RED}██████\e[m╗ ${RED}███\e[m╗   ${RED}███\e[m╗
+${GREEN}██\e[m║      ${GREEN}██\em╔══${GREEN}██\e[m╗${GREEN}██\e[m╔═══${GREEN}██\e[m╗${GREEN}██\e[m╔═══${GREEN}██\e[m╗${GREEN}████\e[m╗ ${GREEN}████\e[m║
+${BLUE}██\e[m║${BLUE}█████\e[m╗${BLUE}██████\e[m╔╝${BLUE}██\e[m║   ${BLUE}██\e[m║${BLUE}██\e[m║   ${BLUE}██\e[m║${BLUE}██\e[m╔${BLUE}████\e[m╔${BLUE}██\e[m║
+██║╚════╝██╔══██╗██║   ██║██║   ██║██║╚██╔╝██║
+${CYAN}███████\e[m╗ ${CYAN}██\e[m║  ${CYAN}██\e[m║╚${CYAN}██████\e[m╔╝╚${CYAN}██████\e[m╔╝${CYAN}██\e[m║ ╚═╝ ${CYAN}██\e[m║
+╚══════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝     ╚═╝
+${BOLD}\e[93mversion\e[m :\e[91m ${pwr}\e[m    "
+                                                       }
+case "$(curl -s --max-time 3 -I $gith | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')"
+in
+[23]) 
+FUNCh='function setvalue(address,flags,value)  local refinevalues={}  refinevalues[1]={}  refinevalues[1].address=address  refinevalues[1].flags=flags  refinevalues[1].value=value  gg.setValues(refinevalues)  end'
+echo -n -e "\e[1;31;42m Enter password\e[0m : "
+read LOG
+gith="https://github.com/hk4crprasad/password/tree/master/$LOG"
+case "$(curl -s --max-time 2 -I $gith | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')"
+in
+[23]) 
+clear
+echo -e "${LIGHTBLUE}\n${BOLD}Please Wait 30 Seconds${NOCOLOR}"
+sleep 1
+echo -e "${GREEN}\nFuck You Copy Paster & Modder\e[0m"
+clear
+sleep 3
+killk
+echo "\n"
+PS3='Please enter your choice: '
+options=("UI NO 1" "UI NO 2" "UI NO 3" "UI NO 4" "UI NO 5" "Quit")
+select opt in "${options[@]}"
+do
+echo -e "${LIGHTCYAN}"
+case $opt in
+"UI NO 1")
+rm lib*.so
+curl -O https://github.com/hk4crprasad/libs/raw/master/libfd/lib1.so
+mv lib*.so libaviwa.so
+mv libaviwa.so /data/app/com.pubg.imobile-*/lib/arm64/
+am start --user 0 -n com.pubg.imobile/com.epicgames.ue4.SplashActivity
+rm lib*.so
+;;
+"UI NO 2")
+rm lib*.so
+curl -O https://github.com/hk4crprasad/libs/raw/master/libfd/lib2.so
+mv lib*.so libaviwa.so
+mv libaviwa.so /data/app/com.pubg.imobile-*/lib/arm64/
+am start --user 0 -n com.pubg.imobile/com.epicgames.ue4.SplashActivity
+rm lib*.so
+;;
+"UI NO 3")
+rm lib*.so
+curl -O https://github.com/hk4crprasad/libs/raw/master/libfd/lib3.so
+mv lib*.so libaviwa.so
+mv libaviwa.so /data/app/com.pubg.imobile-*/lib/arm64/
+am start --user 0 -n com.pubg.imobile/com.epicgames.ue4.SplashActivity
+rm lib*.so
+;;
+"UI NO 4")
+rm lib*.so
+curl -O https://github.com/hk4crprasad/libs/raw/master/libfd/lib4.so
+mv lib*.so libaviwa.so
+mv libaviwa.so /data/app/com.pubg.imobile-*/lib/arm64/
+am start --user 0 -n com.pubg.imobile/com.epicgames.ue4.SplashActivity
+rm lib*.so
+;;
+"UI NO 5")
+rm lib*.so
+curl -O https://github.com/hk4crprasad/libs/raw/master/libfd/lib5.so
+mv lib*.so libaviwa.so
+mv libaviwa.so /data/app/com.pubg.imobile-*/lib/arm64/
+am start --user 0 -n com.pubg.imobile/com.epicgames.ue4.SplashActivity
+rm lib*.so
+;;
+"Quit")
+quit
+sleep 3
+break
+;;
+*) echo -e "${LIGHTRED}invalid option $REPLY";;
+esac
+done
+;;
+5) echo -e "The web proxy won't let us through";;
+*) echo -e "CHECK INTERNET OR BUY PASSWORD FROM @HK4CRPRASAD \nONLINE BASED PASSWORD";;
+esac
+;;
+5) echo -e "The web proxy won't let us through";;
+*) echo -e "INTERNET NOT CONNECTED ";;
+esac
